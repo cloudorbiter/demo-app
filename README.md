791a9c3d59bb8893483cca3eeac0130f76231de3
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2023-10-13 17:31:31 +0000

    make python3.8 compatible
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A Sample demo app for showcasing native app capability of cloud orbiter.
 
 Application is a sample app running a http server to just dump the version on browser page
+
+environment: python3.8
--- app.py
+++ app.py
@@ -1,33 +1,36 @@
-import BaseHTTPServer
+import http.server
 import time
 import os
-from SocketServer import ThreadingMixIn
+from socketserver import ThreadingMixIn
 
 PORT_NUMBER = 8080 # Maybe set this to 9000.
 
 VERSION = "v1.0"
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def write_message(s, msg):
+        s.wfile.write(msg.encode())
+
     def do_GET(s):
         """Respond to a GET request."""
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
-        s.wfile.write("<html><head><title>Coredge.io Demo app</title></head>")
-        s.wfile.write("<body><h2>Demo App Version:- " + os.environ.get('VERSION', VERSION) + "</h2>")
-        s.wfile.write("</body></html>")
+        s.write_message("<html><head><title>Coredge.io Demo app</title></head>")
+        s.write_message("<body><h2>Demo App Version:- " + os.environ.get('VERSION', VERSION) + "</h2>")
+        s.write_message("</body></html>")
 
-class ThreadingHTTPServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+class ThreadingHTTPServer(ThreadingMixIn, http.server.HTTPServer):
     pass
 
 if __name__ == '__main__':
     #server_class = BaseHTTPServer.HTTPServer
     server_class = ThreadingHTTPServer
     httpd = server_class(("", PORT_NUMBER), MyHandler)
-    print time.asctime(), "Server Starts - :%s" % (PORT_NUMBER)
+    print(time.asctime(), "Server Starts - :%s" % (PORT_NUMBER))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    print time.asctime(), "Server Stops - :%s" % (PORT_NUMBER)
+    print(time.asctime(), "Server Stops - :%s" % (PORT_NUMBER))
